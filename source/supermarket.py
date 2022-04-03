a69bdf2a420229e87f5bee27bb0e30212eff618c
0_0_0_ <57903674+llz200@users.noreply.github.com>
2022-04-03 23:35:46 +0800

    Add files via upload

--- source/supermarket.py
+++ source/supermarket.py
@@ -1,4 +1,4 @@
-# 导入模块
+#导入模块
 
 from operator import index
 import sys
@@ -6,33 +6,32 @@ from turtle import towards
 import pygame
 import math
 
-# pygame初始化
+#pygame初始化
 
 pygame.init()
 
-# 设置主窗口
+#设置主窗口
 
-main_screen = pygame.display.set_mode((1280, 960))
+main_screen = pygame.display.set_mode((1280,960))
 pygame.display.set_caption("这是一个标题")
 image_icon = pygame.image.load("../res/image/icon.png").convert()
 pygame.display.set_icon(image_icon)
 
-# 设置背景
+#设置背景
 
 image_background = pygame.image.load("../res/image/map.png").convert()
 background_x = 320
 background_y = -1280
 
-# 初始化移动变量
+#初始化移动变量
 
 movement_x = 0
 movement_y = 0
 
-# 定义实体类
-
+#定义实体类
 
 class solid(pygame.sprite.Sprite):
-    def __init__(self, filename, location):
+    def __init__(self,filename,location):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(filename)
         self.rect = self.image.get_rect()
@@ -76,28 +75,116 @@ class player(pygame.sprite.Sprite):
             self.last_time = current_time
         if self.index >= y:
             self.index = x - 1
-# 设置角色
 
-
+#设置角色
+        
 buyer = player("../res/image/人物.png", 12)
 buyer_speed = 12
 max_speed = 12
 min_speed = 6
 
+#设置物品参数
+#饮料 食物 书 杂项 鱼 水果
 
-# 设置障碍物
+item_volume = [1,1,2,3,5,5]
+
+#设置背包
+
+bag_volume = 10
+bag_left = 10
+bag_item = [0,0,0,0,0,0]
+
+#设置战果
+
+result_item = [0,0,0,0,0,0]
+
+#设置障碍物和柜子
 
 blocks = []
-for i in range(7, 26):
-    blocks.append(solid("../res/image/wrong.png", (160*i+320, 160*3-1280)))
-for i in range(7, 22):
-    blocks.append(solid("../res/image/wrong.png", (160*i+320, 160*15-1280)))
 
-# 创建时钟对象（控制游戏的FPS）
+shelf = []
+for i in range(0,6):
+    shelf.append([])
+
+for i in range(8,26):
+    blocks.append(solid("../res/image/wrong.png" , (160*i+320,160*3-1280)))
+for i in range(10,20):
+    shelf[0].append(solid("../res/image/wrong.png" , (160*i+320,160*4-1280-80)))
+    blocks.append(solid("../res/image/wrong.png" , (160*i+320,160*7-1280)))
+    shelf[1].append(solid("../res/image/wrong.png" , (160*i+320,160*8-1280-80)))
+for i in range(8,18):
+    blocks.append(solid("../res/image/wrong.png" , (160*i+320,160*11-1280)))
+    shelf[3].append(solid("../res/image/wrong.png" , (160*i+320,160*12-1280-80)))
+for i in range(22,26):
+    blocks.append(solid("../res/image/wrong.png" , (160*i+320,160*5-1280)))
+    shelf[2].append(solid("../res/image/wrong.png" , (160*i+320,160*6-1280-80)))
+    blocks.append(solid("../res/image/wrong.png" , (160*i+320,160*9-1280)))
+    shelf[2].append(solid("../res/image/wrong.png" , (160*i+320,160*10-1280-80)))
+for i in range(4,13):
+    blocks.append(solid("../res/image/wrong.png" , (160*26+320,160*i-1280)))
+for i in range(8,19):
+    blocks.append(solid("../res/image/wrong.png" , (160*i+320,160*15-1280)))
+for i in range(4,8):
+    blocks.append(solid("../res/image/wrong.png" , (160*i+320,160*6-1280)))
+for i in range(7,10):
+    blocks.append(solid("../res/image/wrong.png" , (480+320,160*i-1280)))
+for i in range(12,13):
+    blocks.append(solid("../res/image/wrong.png" , (480+320,160*i-1280)))
+for i in range(4,6):
+    blocks.append(solid("../res/image/wrong.png" , (160*i+320,160*13-1280)))
+for i in range(20,26):
+    blocks.append(solid("../res/image/wrong.png" , (160*i+320,160*13-1280)))
+for i in range(6,8):
+    shelf[4].append(solid("../res/image/wrong.png" , (160*i+320,160*14-1280)))
+for i in range(19,20):
+    shelf[5].append(solid("../res/image/wrong.png" , (160*i+320,160*14-1280)))
+for i in range(4,6):
+    blocks.append(solid("../res/image/wrong.png" , (160*7+320,160*i-1280)))
+for i in range(1,3):
+    blocks.append(solid("../res/image/wrong.png" , (160*i+320,160*9-1280)))
+for i in range(1,3):
+    blocks.append(solid("../res/image/wrong.png" , (160*i+320,160*12-1280)))
+for i in range(10,12):
+    blocks.append(solid("../res/image/wrong.png" , (320,160*i-1280)))
+
+
+#定义碰撞函数
+
+def solid_collide_y(a,m_y,b):
+    for i in a:
+        i.rect.top += m_y
+        if pygame.sprite.collide_rect(b , i):
+            i.rect.top -= m_y
+            return 0
+        i.rect.top -= m_y
+    return 1
+
+def solid_collide_x(a,m_x,b):
+    for i in a:
+        i.rect.left += m_x
+        if pygame.sprite.collide_rect(b , i):
+            i.rect.left -= m_x
+            return 0
+        i.rect.left -= m_x
+    return 1
+
+def solid_collide_xy(a,m_x,m_y,b):
+    for i in a:
+        i.rect.left += m_x
+        i.rect.top += m_y
+        if pygame.sprite.collide_rect(b , i):
+            i.rect.left -= m_x
+            i.rect.top -= m_y
+            return 0
+        i.rect.left -= m_x
+        i.rect.top -= m_y
+    return 1
+            
+#创建时钟对象（控制游戏的FPS）
 
 clock = pygame.time.Clock()
 
-# 主循环
+#主循环
 
 while True:
 
@@ -135,14 +222,22 @@ while True:
             movement_x = -buyer_speed
             buyer.towards = 1
             buyer.movement = 1
-        if pygame.key.get_pressed()[pygame.K_SPACE]:
-            if  96 <= background_x <= 480 and -1376 <= background_y <= -1184:
-                for i in range(0,4):
-                    result_item[i] += bag_item[i]
-                    bag_item[i] = 0
-            flag = 1
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if  96 <= background_x <= 480 and -1376 <= background_y <= -1184:
+                    for i in range(0,6):
+                        result_item[i] += bag_item[i]
+                        bag_item[i] = 0
+                        bag_left = bag_volume
+                        print(result_item[i])
+                for j in range(0,6):
+                    if (solid_collide_y(shelf[j],max_speed,buyer) == 0):
+                        if bag_left >= item_volume[j]:
+                            bag_left -= item_volume[j]
+                            bag_item[j] += 1
+                        break
             
-
+  
 #处理上下碰撞
 
     if (movement_x == 0 and movement_y != 0):
@@ -248,6 +343,6 @@ while True:
     else:
         buyer.index = buyer.firstframe[buyer.towards]
     buyer.image = buyer.images[buyer.index]
-    main_screen.blit(buyer.image, buyer.rect)
+    main_screen.blit( buyer.image , buyer.rect )
     
     pygame.display.flip()
