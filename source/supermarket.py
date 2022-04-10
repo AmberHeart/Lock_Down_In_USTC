fe37bacf49aea75fb2b5fd82926a76796e613327
fishsure <1055381810@qq.com>
2022-04-10 11:49:29 +0800

    鼠标跳过功能

--- source/supermarket.py
+++ source/supermarket.py
@@ -1,6 +1,7 @@
 #导入模块
 import sys
 import pygame
+import pygame.freetype
 import math
 from pause import GamePause
 
@@ -47,8 +48,24 @@ class Supermarket:
                 self.image = pygame.image.load(filename)
                 self.rect = self.image.get_rect()
                 self.rect.topleft = location
-                
 
+        def word_print(space_limit ,text, font, color=(255, 255, 255)):
+            font.origin = True
+            #words = text.split()
+            l_left = space_limit[0]
+            l_right = space_limit[1]
+            l_up = space_limit[2]
+            l_down = space_limit[3]
+            line_spacing = font.get_sized_height() + 2
+            x, y = l_left, line_spacing + l_up
+            space = font.get_rect(' ')
+            for word in text:
+                bounds = font.get_rect(word)
+                if x + bounds.width + bounds.x >= l_right:
+                    x, y = l_left, y + line_spacing
+                font.render_to(self, (x, y), None, color)
+                x += bounds.width + space.width        
+        
         # 定义玩家类
 
 
@@ -102,6 +119,8 @@ class Supermarket:
         font3 = pygame.font.Font("../res/font/Pixel.ttf",45)
         black = (0, 0, 0)
         white = (255, 255, 255)
+        passfont = pygame.freetype.Font("../res/font/Pixel.ttf",30)
+        passfont.antialiased = False
 
         #设置物品参数
         #饮料 食物 书 杂项 鱼 水果
@@ -282,6 +301,40 @@ class Supermarket:
             for j in range(0,6):
                 for i in shelf[j]:
                     solid_move(i,m_x,m_y)
+        #按钮类
+        class button(pygame.sprite.Sprite):
+            def __init__(self,filename1,location,font,text):
+                pygame.sprite.Sprite.__init__(self)
+                self.image = (pygame.image.load(filename1))
+                self.rect = self.image.get_rect()
+                self.rect.center = location
+                self.text = text
+                self.font = font
+                self.color = (0,0,0)
+            def update(self):
+                pos = pygame.mouse.get_pos()
+                lpos = self.rect.center[0] - self.image.get_width()/2
+                rpos = self.rect.center[0] + self.image.get_width()/2
+                upos = self.rect.center[1] - self.image.get_height()/2
+                dpos = self.rect.center[1] + self.image.get_height()/2
+                if pos[0] > lpos and pos[0] < rpos and pos[1] > upos and pos[1] < dpos:
+                    if self.color == (0,0,0):
+                        self.color = (255,255,255)
+                    buttons = pygame.mouse.get_pressed()
+                    if buttons[0]:
+                        return 1
+                    else:
+                        return 0
+                else:
+                    self.color = (0,0,0)
+            def print(self,screen):
+                screen.blit(self.image, self.rect)
+                lpos = self.rect.center[0] - self.image.get_width()/2
+                rpos = self.rect.center[0] + self.image.get_width()/2
+                upos = self.rect.center[1] - self.image.get_height()/2
+                dpos = self.rect.center[1] + self.image.get_height()/2
+                word_print((lpos +20,rpos - 20 , upos +10 , dpos -10), self.text , self.font ,self.color)
+        passbutton = button("../res/image/选项按钮.png" , (1100 , 750) , passfont , "跳过" )
 
         #创建时钟对象（控制游戏的FPS）
 
@@ -326,6 +379,9 @@ class Supermarket:
                             time0 += tmptime1 - tmptime0
                     #pygame.event.pump()
             if clock_ <= 60:
+                passbutton.print(self)
+                if passbutton.update() == 1:
+                    clock_ = -1
                 for event in pygame.event.get():
                     if event.type == pygame.WINDOWFOCUSLOST:
                         tmptime0 = pygame.time.get_ticks()
