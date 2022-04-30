20172c624917391e4ba4737068ebe5f755c9eea8
Silent-yx <2421162315@qq.com>
2022-04-30 17:32:11 +0800

    加了两个事件

--- source/eventlist.py
+++ source/eventlist.py
@@ -11,7 +11,7 @@ class EventList:
             self.resulttext = resulttext
 
     #事件总数，每次加事件的时候记得改
-    event_num = 13
+    event_num = 15
     
     evelist = []
     #属性增减 饥饿值 口渴值 san值 智商 清洁值 时间(单位15min) (单个事件的时间不要超过37)
@@ -20,38 +20,38 @@ class EventList:
     limit = []
     #做出选择后在状态栏出现的信息
     message = []
-    #刷出该事件的需求，无要求为0 最后两项为时间下限和上限 范围在32~88之间 无要求则同时为0
+    #刷出该事件的需求，无要求为0 最后两项为时间下限和上限 范围在32~88（即8~22点）之间 无要求则同时为0
     refreshneed = []
 
     #! 有特殊影响的事件在dormitory中的specialeffect处添加！！！
     
     #时间对照: ( 8:00 -> 32 ) ( 22:00 -> 88 )
         
-    #0
+    #0 翘课事件
     evelist.append(eve("../res/image/要迟到了.png","早上睡迟了，要不要翘课呢",2 ,["翘！","不翘！"] , ["翘的好！","冲去上课，满身是汗"]))
     effect.append([[0,0,0,-1,0,4],[0,0,0,0,-1,4]])
     limit.append([[0,0,0,1,0],[0,0,0,0,0]])
     message.append(["翘课睡觉咯，智商-1","冲去上课，满身是汗，清洁值-1"])
     refreshneed.append([0,0,0,0,0,32,40])
-    #1
+    #1 老坛酸菜  
     evelist.append(eve("../res/image/老坛酸菜.png","隔壁老铁送来老坛酸菜牛肉面，要不要吃捏",2 ,["吃","不吃！"] , ["就这味儿！干净又卫生！","..."]))
     effect.append([[+1,0,0,0,-1, 1],[0,0,0,0,0, 1]])
     limit.append([[0,0,0,0,1],[0,0,0,0,0]])
     message.append(["吃了老坛酸菜牛肉面，饥饿值+1，清洁值-1","谢绝了隔壁老铁的老坛酸菜牛肉面"])
-    refreshneed.append([0,0,0,0,0,0,0])
-    #2
+    refreshneed.append([0,0,0,0,0,50,80])  #小改了一下时间，老坛频率太高有点掉SAN
+    #2 零星小睡
     evelist.append(eve("../res/image/睡觉.png","小咪亿下",1 ,["睡！"] , ["zzZZZZZZZZ..."]))
     effect.append([[0,0,0,0,0,36]])
     limit.append([[0,0,0,0,0]])
     message.append(["小睡了9个小时"])
     refreshneed.append([0,0,0,0,0,0,0])
-    #3
+    #3 夜间读书
     evelist.append(eve("../res/image/夜间读书.png","要熄灯了，要不要偷偷读书呢",2 ,["偷偷读","直接睡觉了"] , ["拿着手电筒在被窝里读书，可惜效率不高","zzZZZZZZ"]))
     effect.append([[0,0,-2,1,0,8],[0,0,0,0,1,4]])
     limit.append([[0,0,2,0,0],[0,0,0,0,0]])
     message.append(["秉灯夜读，san值-2，智商+1","刷牙洗脸睡咯，清洁值+1"])
     refreshneed.append([0,0,0,0,0,84,88])
-    #4
+    #4 自习室事件
     evelist.append(eve("../res/image/自习室.png","好朋友邀你去自习室卷卷",2 ,["出发！","不去，在床上摆烂了"] , ["自习了好久，感觉学会了不少东西呢","摸鱼摸的好爽"]))
     effect.append([[0,0,0,2,0,12],[0,0,2,-2,0,4]])
     limit.append([[0,0,0,0,0],[0,0,0,0,0]])
@@ -105,3 +105,16 @@ class EventList:
     limit.append([[0,0,0,0,0],[0,0,0,0,0]])
     message.append(["喝了旺仔牛奶，口渴值+1","吃了旺旺粟米条，不管饱，口渴值-1"])
     refreshneed.append([0,0,0,0,0,48,88])
+    #13 小霸王游戏机
+    evelist.append(eve("../res/image/小霸王.png","朋友发来一个链接打开后发现是小霸王", 2 ,["开玩！","算了，还是学习吧"] , ["对大学生来说刚刚好！","学习使我快乐捏~"]))
+    effect.append([[0,0,+1,-1,0, 8],[0,0,-1,+1,0, 8]])
+    limit.append([[0,0,0,0,0],[0,0,0,0,0]])
+    message.append(["玩了两小时 智商-1  san值+1","学了两小时 san值-1 智商+1"])
+    refreshneed.append([0,0,0,0,0,0,0])
+    #14 夜聊事件
+    evelist.append(eve("../res/image/夜聊.png","晚上收到了消息", 2 ,["发生甚么事了","假装没看到睡大觉"] , ["聊了很晚，困死了","睡完大觉感到愧疚"]))
+    effect.append([[0,0,0,-1,0, 12],[0,0,-1,0,0, 12]])
+    limit.append([[0,0,0,0,0],[0,0,0,0,0]])
+    message.append(["好困啊 智商-1","有点愧疚 san值-1"])
+    refreshneed.append([0,0,0,0,0,0,0])
+    #15 
